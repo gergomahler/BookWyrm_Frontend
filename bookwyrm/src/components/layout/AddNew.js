8d762029c72158706acf359b2c1c1c175a728438
gergomahler <49788003+gergomahler@users.noreply.github.com>
2020-10-15 23:05:16 +0200

    using grid in addnew

--- bookwyrm/src/components/layout/AddNew.js
+++ bookwyrm/src/components/layout/AddNew.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { Grid } from "@material-ui/core";
 
 const Sprite = styled.img`
   width: 200px;
@@ -19,30 +20,26 @@ function AddNew(props) {
 
     return (
         <div className="AddNew">
-            <div className="row">
-                <div className="col">
-                    <div className="row">
-                        <div className="card">
-                            <StyledLink to="/addnew/book">
-                            <Sprite src={process.env.PUBLIC_URL + '/img/bookicon.png'}></Sprite>
-                            <h4>New Book</h4>
-                            </StyledLink>
-                        
-                        
-                        <StyledLink to="/addnew/author">
-                            <Sprite src={process.env.PUBLIC_URL + '/img/authoricon.png'}></Sprite>
-                            <h4>New Author</h4>
-                            </StyledLink>
-                            
-                        
-                        <StyledLink to="/addnew/publisher">
-                            <Sprite src={process.env.PUBLIC_URL + '/img/publishericon.png'}></Sprite>
-                            <h4>New Publisher</h4>
-                            </StyledLink>
-                            </div>
-                     </div>
+            <Grid container direction="row" justify="center" alignItems="center">
+                <div className="addcard">
+                    <StyledLink to="/addnew/book">
+                        <Sprite src={process.env.PUBLIC_URL + '/img/bookicon.png'}></Sprite>
+                        <h4>New Book</h4>
+                    </StyledLink>
                 </div>
-            </div>
+                <div className="addcard">
+                    <StyledLink to="/addnew/author">
+                        <Sprite src={process.env.PUBLIC_URL + '/img/authoricon.png'}></Sprite>
+                        <h4>New Author</h4>
+                    </StyledLink>
+                </div>
+                <div className="addcard">
+                    <StyledLink to="/addnew/publisher">
+                        <Sprite src={process.env.PUBLIC_URL + '/img/publishericon.png'}></Sprite>
+                        <h4>New Publisher</h4>
+                    </StyledLink>
+                </div>
+            </Grid>
         </div>
     );
 }
